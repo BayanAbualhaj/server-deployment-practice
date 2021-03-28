ee2ab8920b218df9742d311b97be908c54670a13
BayanAbualhaj <bayanabualhaj1097@gmail.com>
2021-03-28 16:50:22 +0300

    branch

---
+++ __tests__/server.test.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const { it, expect } = require('@jest/globals');
+const superTest = require('supertest');
+const { describe } = require('yargs');
+const server = require('../server.js');
+const request = superTest(sever.app);
+
+describe('server',()=>{
+    it('handle invalid routs',async()=>{
+        const response = await request.get('/foo');
+        expect(response.status).toEqual(404);
+    });
+    it('handle server error',async()=>{
+        const response = await request.get('/bad');
+        expect(response.status).toEqual(500);
+    });
+    it('handle working routes',async()=>{
+        const response = await request.get('/');
+        expect(response.text).toEqual('Hello There');
+    });
+});
